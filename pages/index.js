4c527392492bcb1fe76b55ac0121e99225e43705
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-02-22 18:23:31 +0100

    fix index

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
 	return (
 	<main
-		className={`bg-orange-300 text-white flex min-h-screen flex-col items-center justify-center p-12 text-8xl`}>
+		className={`bg-orange-300 w-screen h-screen text-white flex flex-col items-center justify-center text-3xl md:text-6xl`}>
 		ShapeBook
 		<Link href="/step1">
 			{"-->"}
