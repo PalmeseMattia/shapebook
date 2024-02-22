82d1760c2e2ac2f1ad7399511cbcc8f80ddea024
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-02-22 18:00:31 +0100

    arancione pagina iniziale

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
 	return (
 	<main
-		className={`flex min-h-screen flex-col items-center justify-center p-12 text-8xl`}>
+		className={`bg-orange-300 text-white flex min-h-screen flex-col items-center justify-center p-12 text-8xl`}>
 		ShapeBook
 		<Link href="/step1">
 			{"-->"}
