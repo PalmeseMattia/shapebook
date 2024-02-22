e5346dadc90f00c61cc54a0551ac019a3eda94be
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-02-22 18:10:15 +0100

    fix frame

--- components/Frame.js
+++ components/Frame.js
@@ -7,12 +7,12 @@ function Frame({data}) {
 		<Image src={data.img} fill={false} className='object-contain' priority={true} alt=""></Image>
 		{/* Quadratino importante */}
 		<div 
-			className={`absolute bg-white rounded-full overflow-clip`} 
+			className={`absolute bg-white rounded-full overflow-clip items-center justify-center flex`} 
 			style={{ left: `${data.left1}%`, top: `${data.top1}%`, width: `${data.width1}%`, height: `${data.height1}%` }}>
 				<img src={data.screenshot[0]} className='w-[180%] h-[180%] object-cover'></img>
 		</div>
 		<div 
-			className={`absolute bg-white rounded-full overflow-clip`} 
+			className={`absolute bg-white rounded-full overflow-clip items-center justify-center flex`} 
 			style={{ left: `${data.left2}%`, top: `${data.top2}%`,  width: `${data.width2}%`, height: `${data.height2}%` }}>
 				<img src={data.screenshot[1]} className='w-[180%] h-[180%] object-cover'></img>
 		</div>
